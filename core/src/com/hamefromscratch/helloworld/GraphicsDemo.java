593a47257fcf0aa97a15c7571aa18b36642ff1c6
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-29 14:56:20 +0000

    fix assets path

--- core/src/com/hamefromscratch/helloworld/GraphicsDemo.java
+++ core/src/com/hamefromscratch/helloworld/GraphicsDemo.java
@@ -1,6 +1,7 @@
 package com.hamefromscratch.helloworld;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,9 +13,15 @@ public class GraphicsDemo implements ApplicationListener {
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+
 	
 	@Override
 	public void create() {
+        Gdx.app.log("AssetPath", Gdx.files.internal("data/image.png").file().getAbsolutePath());
+        batch = new SpriteBatch();
+        //texture = new Texture(Gdx.files.internal("data/image.png"));
+        texture = new Texture(Gdx.files.getFileHandle("data/image.png", Files.FileType.Internal));
+        sprite = new Sprite(texture);
 
 	}
 
@@ -28,9 +35,6 @@ public class GraphicsDemo implements ApplicationListener {
 	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch = new SpriteBatch();
-        texture = new Texture(Gdx.files.internal("data/image.png"));
-        sprite = new Sprite(texture);
 		
 		batch.begin();
 		sprite.draw(batch);
